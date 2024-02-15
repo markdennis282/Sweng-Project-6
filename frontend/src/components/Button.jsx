e76f3064399eea3531f21c58965f32859e675a28
Michal Bronicki <bronickm@tcd.ie>
2024-02-15 15:58:04 +0100

    updates Button components to use css modules

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,19 +1,14 @@
-// Button.jsx
-import "./Button.css";
 import PropTypes from "prop-types";
 
+import styles from "./Button.module.css";
+
 function Button({ text, highlighted, className, ...props }) {
-    const style = highlighted ? "navButton highlighted" : "navButton";
-    const fullClassName = className ? `${style} ${className}` : style;
+    let cssStylesClassNames = styles.button;
+    if(highlighted) cssStylesClassNames += ` ${styles.highlighted}`;
+    const fullClassName = className ? `${cssStylesClassNames} ${className}` : cssStylesClassNames;
+
     return <button className={fullClassName} {...props}>{ text }</button>;
 }
-
-// Button.propTypes = {
-//     name: PropTypes.string.isRequired,
-//     title: PropTypes.string.isRequired,
-//     onClick: PropTypes.func,
-//     status: PropTypes.string
-// };
 
 Button.propTypes = {
     text: PropTypes.string.isRequired,
