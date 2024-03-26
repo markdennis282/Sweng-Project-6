a06ae5ddf76079ff79204e2a5d42f6f213fb74b3
Audrey <delrosaa@tcd.ie>
2024-03-26 11:09:38 +0000

    comment changes is chatbox test

--- frontend/src/components/ChatBox.test.jsx
+++ frontend/src/components/ChatBox.test.jsx
@@ -31,17 +31,6 @@ describe ('ChatBox Component', () => {
         expect(await screen.findByText("Hi! How can I help you")).toBeInTheDocument();
     });
 
-<<<<<<< Updated upstream
-
-    //TODO
-    // check 'not relevant' message 
-
-    // check API failure error message 
-    
-    
-
-=======
->>>>>>> Stashed changes
     // messages added correctly 
     it ("should add the user message and clear the input when Enter key is pressed", async() => {
         render(<ChatBox sourceTag={sourceTag} />);
@@ -61,18 +50,8 @@ describe ('ChatBox Component', () => {
         expect(screen.getByText(sourceTag)).toBeInTheDocument();
     });
 
-<<<<<<< Updated upstream
-
-    //TODO
-    // messages scrolls to bottom
-    it("scrolls to the bottom when new message is added", async() => {
-
-
-    });
-=======
     //TODO
     // messages scrolls to bottom - can't figure out how to test LLM response to trigger scrollInView 
->>>>>>> Stashed changes
     
 
 });
