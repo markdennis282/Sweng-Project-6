b6623a8a9f8a9a6882c9d53c2fceeafa5980c7f3
Michal Bronicki <bronickm@tcd.ie>
2024-04-05 16:19:31 +0200

    adds necessary modifications to ChatBox component and fixes tests

--- frontend/src/components/ChatBox.test.jsx
+++ frontend/src/components/ChatBox.test.jsx
@@ -1,9 +1,9 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import axios from "axios";
 
 import ChatBox from "./ChatBox";
+import * as ApiAccess from "../utils/apiAccess";
 
 /*
     things to test
@@ -15,7 +15,6 @@ import ChatBox from "./ChatBox";
 
 Element.prototype.scrollIntoView = jest.fn();
 
-jest.mock("axios");
 jest.mock("../utils/apiAccess");
 
 describe("ChatBox Component", () => {
@@ -23,8 +22,7 @@ describe("ChatBox Component", () => {
 
     // dependencies - API call
     it("should handle API response and display AI message", async () => {
-        const mockResponse = { data: { response: "Hi! How can I help you" } };
-        axios.post.mockResolvedValueOnce(mockResponse);
+        const getChatStreamResponseSpy = jest.spyOn(ApiAccess, "getChatStreamResponse");
 
         render(<ChatBox sourceTag={sourceTag} />);
         const input = screen.getByPlaceholderText("Type your query and hit enter...");
@@ -32,14 +30,13 @@ describe("ChatBox Component", () => {
         // simulate user message ('Hello') and Enter key pressed
         await userEvent.type(input, "Hello {enter}");
 
-        expect(await screen.findByText("Hi! How can I help you")).toBeInTheDocument();
+        expect(getChatStreamResponseSpy).toHaveBeenCalledTimes(1);
+
+        expect(await screen.findByText(/Final response/)).toBeInTheDocument();
     });
 
     // messages added correctly
     it("should add the user message and clear the input when Enter key is pressed", async () => {
-        const mockResponse = { data: { response: "Hi! How can I help you" } };
-        axios.post.mockResolvedValueOnce(mockResponse);
-
         render(<ChatBox sourceTag={sourceTag} />);
         const input = screen.getByPlaceholderText("Type your query and hit enter...");
 
@@ -48,13 +45,12 @@ describe("ChatBox Component", () => {
 
         expect(screen.getByText("Hello")).toBeInTheDocument(); // input to appear as message on screen
         expect(input).toHaveValue(""); // clear input
-
     });
 
     // sourceTag is being used correctly
     it("should show initial message from on sourceTag", () => {
         render(<ChatBox sourceTag={sourceTag} />);
-        expect(screen.getByText(sourceTag)).toBeInTheDocument();
+        expect(screen.getByText(new RegExp(sourceTag))).toBeInTheDocument();
     });
 
     // TODO
