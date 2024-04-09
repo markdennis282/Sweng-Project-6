057b9b50134c11a94066fa28bd0852095e39cd71
dcipani <33790535+Dcipani@users.noreply.github.com>
2024-04-09 16:40:45 +0100

    UI updates to landing page

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -11,6 +11,8 @@ import nancy from "../assets/headshots/nancy.jpeg";
 import inam from "../assets/headshots/inam.jpeg";
 import mark_d from "../assets/headshots/mark_d.jpeg";
 import creagh from "../assets/headshots/creagh.jpeg";
+import ruslan from "../assets/headshots/ruslan.jpeg";
+import pierce from "../assets/headshots/pierce.jpeg";
 
 function LandingPage({ onButtonClick }) {
     const handleClick = () => {
@@ -20,8 +22,8 @@ function LandingPage({ onButtonClick }) {
         <div className={styles.landingPageContainer}>
             <div className={styles.botHeader}>meet millennium</div>
             <div className={styles.logoContainer}>
-                <svg width="100%" height="150px">
-                    <text x="140px" y="60%" fill="white" onClick={handleClick}>marvin.</text>
+                <svg width="100%" height="100px">
+                    <text x="140px" y="80%" fill="white" onClick={handleClick}>marvin.</text>
                 </svg>
             </div>
             <div className={styles.aboutText}> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere illo minus culpa mollitia unde commodi inventore voluptatem obcaecati molestias, rerum molestiae neque sapiente nesciunt odio beatae iure non, sequi deserunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita quis error deleniti itaque voluptatum possimus qui aperiam perferendis alias nesciunt ea doloremque accusantium doloribus animi illum quasi quaerat, libero ut?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, qui! Temporibus aspernatur quos mollitia molestias quidem obcaecati. Eaque accusantium fugit voluptate culpa, doloribus est molestias aut ea dolore maxime pariatur!</div>
@@ -40,8 +42,8 @@ function LandingPage({ onButtonClick }) {
                         <Headshot info={["Nancy Heppe", "2nd Year", "Backend"]} image={nancy} />
                         <Headshot info={["Audrey Del Rosario", "2nd Year", "Frontend"]} image={audrey} />
                         <Headshot info={["Inam Syed", "2nd Year", "Frontend"]} image={inam} />
-                        <Headshot info={["Ruslan Kirniev", "2nd Year", "LLM"]} image={adel} />
-                        <Headshot info={["Pierce Buckley", "2nd Year", "Frontend"]} image={adel} />
+                        <Headshot info={["Ruslan Kirniev", "2nd Year", "LLM"]} image={ruslan} />
+                        <Headshot info={["Pierce Buckley", "2nd Year", "Frontend"]} image={pierce} />
                     </div>
                 </div>
 
