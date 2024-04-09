48513e6bdb376dfd58f6bd3fd0218b4c0aab00fc
dcipani <33790535+Dcipani@users.noreply.github.com>
2024-04-09 17:59:03 +0100

    fixed img sizing issue

--- frontend/src/components/Headshot.jsx
+++ frontend/src/components/Headshot.jsx
@@ -6,7 +6,7 @@ import styles from "./Headshot.module.css";
 function Headshot({ info, image }) {
     return (
         <div className={styles.headshot}>
-            <img src={image} ></img>
+            <img className={styles.headshot_img} src={image} ></img>
             <div className={styles.info}>
                 {info.map((x, i) => (
                     i === 0 ? <h1 key={i}>{x}</h1> : <p key={i}>{x}</p>
